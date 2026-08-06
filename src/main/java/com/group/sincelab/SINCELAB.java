34ae4de61baadf6926a9dc0cf8d7911c32277bb0
Jose-rade121 <joseenriquemana121@gmail.com>
2026-08-05 19:40:49 -0500

    chore: Initial Project structure

---
+++ src/main/java/com/group/sincelab/SINCELAB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.group.sincelab;
+
+/**
+ *
+ * @author josee
+ */
+public class SINCELAB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
