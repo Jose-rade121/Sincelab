fd8ed193e304f0eedd9922b680f87d28485a7bc3
Jose-rade121 <joseenriquemana121@gmail.com>
2026-08-11 21:44:56 -0500

    style(ui): include header to main GUI

--- src/main/java/sincelab/Main.java
+++ src/main/java/sincelab/Main.java
@@ -10,7 +10,5 @@ public class Main {
     public static void main(String [] args){
         
      mainWindow window = new mainWindow();
-     window.setVisible(true);
-     
     }
 }
--- src/main/java/sincelab/ui/mainWindow.java
+++ src/main/java/sincelab/ui/mainWindow.java
@@ -1,13 +1,16 @@
 package sincelab.ui;
 import javax.swing.*;
 import java.awt.Color;
+//import java.awt.Graphics2D; -> pa luego.
 
 /**
+ *  
+ *
  * 
  *
  * @author josee
  */
-public class mainWindow extends JFrame{
+public final class mainWindow extends JFrame{
     
     public mainWindow() {  
         
@@ -16,11 +19,28 @@ public class mainWindow extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         //Aqui le dice q hace el boton de cerrar(existen muchos otros).
         setResizable(false); //Si la pagina se puede cambiar de tamaño o no.
-        
         getContentPane().setBackground(new Color(226,230,235)); //Cambia el color del fondo
         
-        setVisible(true); //Muestra la ventana.
+        header();
         
+        setVisible(true); //Muestra la ventana. 
     }
+    
+    public void header(){
 
+        JPanel header = new JPanel(); //Crea un panel
+        header.setBackground(new Color(59,130,246));
+        getContentPane().setLayout(null); 
+        //Esto permite que no se remonte sobre los otros
+        header.setBounds(0,0,1280,60);//Altera el tamaño del panel
+        
+        /*
+        * Para redondear usa una libreria de graficos2D
+        * me da cosa que digan q fue con IA entonces mejor pa futuro.
+        *
+        * header.drawRoundRect(0,0,1280,70,20,20);
+        */
+        
+        add(header);//Añade el valor a pantalla
+    }
 }
